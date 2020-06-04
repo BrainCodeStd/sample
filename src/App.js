1f8c31753a5b95722c8548a7d627974f0b2b156e
shomail <shumailmehmood1@gmail.com>
2020-06-04 16:19:33 +0500

    C

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import Header from "./header";
 import './App.css';
 import Section from "./contentSection"
-import Footer from "./footer"
 class App extends Component {
   render() {
     return (
-      <div className="home page-template-default page page-id-68   home" data-template="base.twig">
+      <div className="back">
+
         <Header />
+
+        <div className="row">
+          <div ></div>
+          <div className="video-container">
+            <video
+           width="800px"
+              class="hp__intro-video is-visible"
+              preload="auto"
+              playsinline=""
+              muted="" autoplay=""
+              loop=""
+              data-src="https://www.engine.xyz/wp-content/themes/the-engine-wp-theme/static/video/space"
+              poster="https://www.engine.xyz/wp-content/themes/the-engine-wp-theme/static/video/space--large--poster.jpg">
+              <source
+                class="video-mp4"
+                type="video/mp4; codecs=avc1.42E01E,mp4a.40.2"
+                src="https://www.engine.xyz/wp-content/themes/the-engine-wp-theme/static/video/space--large.mp4" />
+              <source class="video-ogg"
+                type="video/ogg; codecs=theora,vorbis"
+                src="https://www.engine.xyz/wp-content/themes/the-engine-wp-theme/static/video/space--large.ogv" />
+              <source class="video-webm"
+                type="video/webm; codecs=vp8,vorbis"
+                src="https://www.engine.xyz/wp-content/themes/the-engine-wp-theme/static/video/space--large.webm" />
+            </video>
+          </div>
+        </div>
         <Section />
-        <Footer />
       </div>
     );
   }
